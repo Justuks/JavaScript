c958218443e42cf33a636011d22c9adcf6318ccb
Justuks <justinas.valutis@gmail.com>
2022-02-08 10:18:28 +0000

    assign variables for the game

---
+++ assets/game.js
@@ -0,0 +1,11 @@
+//lets start our JS by creating variables
+
+let restartBtn = document.getElementById("restart");
+let prevBtn = document.getElementById("prev");
+let nextBtn = document.getElementById("next");
+let submitBtn = document.getElementById("submit");
+let trueBtn = document.getElementById("True");
+let falseBtn = document.getElementById("False");
+let userScore = document.getElementById("user-score");
+let totalScore = document.getElementById("total-score");
+let questionText = document.getElementById("question-text")
